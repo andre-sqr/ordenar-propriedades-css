44a2372b50745174c9e291e7f6566215b643d43d
andre-sqr <andrebsqr@gmail.com>
2022-07-14 00:37:44 -0300

    update: criação da lista inserida em um método

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ class ListaCSS {
         this.propriedade = [];
     }
 
+    criarLista() {
+        const readline = require('readline');
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+          prompt: 'Digite uma propriedade CSS: '
+        });
+        
+        rl.prompt();
+        rl.on('line', (valor) => {
+            
+            if (valor.toLowerCase() === "sair") {
+                rl.pause()
+                console.log('\nSaindo...\nEssa é sua lista de propriedades:\n');
+                
+                console.log(this.propriedade.sort().join('\n'))
+                process.exit(0);        
+            } else if (valor.trim() === "") {
+                console.log(`Campo vazio! Digite uma propriedade CSS\n`)
+            } else {
+                this.propriedade.push(valor)
+                console.log(valor)
+                console.log(this.propriedade.length)
+                console.log(`\n${valor} adicionado na lista!\n`)
+            }
+            rl.prompt();
+        }).on('close', () => {
+            console.log('Saiu!');
+            process.exit(0);
+        });
+    }
+
     getPropriedade() {
         return this.propriedade;
     }
@@ -12,33 +44,5 @@ class ListaCSS {
     }
 }
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'Digite uma propriedade CSS: '
-});
-
-rl.prompt();
 let listaNova = new ListaCSS();
-rl.on('line', (valor) => {
-    
-    if (valor.toLowerCase() === "sair") {
-        rl.pause()
-        console.log('\nSaindo...\nEssa é sua lista de propriedades:');
-        
-        console.log(listaNova.propriedade.sort())
-        process.exit(0);        
-    } else if (valor.trim() === "") {
-        console.log(`Campo vazio! Digite uma propriedade CSS\n`)
-    } else {
-        listaNova.propriedade.push(valor)
-        console.log(valor)
-        console.log(listaNova.propriedade.length)
-        console.log(`\n${valor} adicionado na lista!\n`)
-    }
-    rl.prompt();
-}).on('close', () => {
-    console.log('Saiu!');
-    process.exit(0);
-});
+listaNova.criarLista()
